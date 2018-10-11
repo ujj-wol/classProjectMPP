5edf2f310a34c9416f8f684f30a8ea05eea7a1b0
tathanhxuan <taxuanus@gmail.com>
2018-10-11 15:12:39 -0500

    Remove redundant code

--- Project4_Students/src/control/LibrarianController.java
+++ Project4_Students/src/control/LibrarianController.java
@@ -15,7 +15,6 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
@@ -33,10 +32,10 @@ import model.domain.CheckoutRecordEntry;
 import model.domain.LibraryMember;
 
 public class LibrarianController {
-	@FXML
-	private Label lblMessage;
+	
 	@FXML
 	private TextField memberID;
+	
 	@FXML
 	private TextField isbnNumber;
 
